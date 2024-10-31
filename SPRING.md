b87d2dfd59698621305a8b3b27cc6bceabcbb362
Lim J <heuristic2022@gmail.com>
2024-10-31 17:43:43 +0900

    rest api 2

--- SPRING.md
+++ SPRING.md
@@ -13,3 +13,15 @@
 - spring.profiles.active=prod : application-prod.properties 이렇게 설정할 수 있음
 - 더 복잡한 애플리케이션의 설정을 저장하려면?
 - 프로퍼티스에서 기본주소 등을 설정하고 오토와이어링 해주면 쓸 수 있음.
+
+- 스프링 프레임워크는 의존성이 핵심. 의존성 주입,식별,자동 연결...
+- modules와 project는 생태계를 확장함.
+- spring MVC의 핵심은 웹앱과 RESTAPI의 빌드 과정을 간소화 하는 것(@Controller,@RestController)
+- spring boot는 project. 빠른 빌드를 도와준다. web starter...Auto configuration...비기능 요구사항(로깅) 등...
+
+- 유저 rest api
+
+  - 검색할 땐 GET으로 /users 만들땐 POST /users 한 명만 검색하고 싶을 땐 GET /users/{id} 한 명만 지우고 싶을 땐 DELETE /users/{id}
+
+- 포스트 rest api
+  - 검색은 GET /users{id}/posts 만들땐 검색은 POST /users{id}/posts 하나만 검색하고 싶을 땐 검색은 GET /users{id}/posts/{post_id}
--- Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,4 +1,27 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-public class HelloWorldController {
+
+import org.springframework.web.bind.annotation.*;
+
+//1. REST API
+@RestController
+public class HelloWorldController
+{
+    //2. url /hello-world
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    //3. output "Hello World"
+    @GetMapping(path = "/hello-world" )
+    public String helloWorld() {
+        return "Hello World";
+    }
+    @GetMapping(path = "/hello-world-bean" )
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+    // /users/{id}/todos/{id}
+
+    @GetMapping(path = "/hello-world/path-variable/{name}" )
+    public HelloWorldBean helloWorldPath(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world, %s",name));
+    }
 }
