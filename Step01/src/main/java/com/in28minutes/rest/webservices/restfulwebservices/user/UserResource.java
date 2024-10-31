3410ddc097a992e5ed1672c82cf98ade27063d45
Lim J <heuristic2022@gmail.com>
2024-10-31 18:00:10 +0900

    rest api user

---
+++ Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,7 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+}
