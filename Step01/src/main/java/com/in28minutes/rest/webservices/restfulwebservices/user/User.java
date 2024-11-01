fa92eb8b2f26c4393d4c99cebcb5b979364e8445
Lim J <heuristic2022@gmail.com>
2024-11-01 19:33:52 +0900

    REST API 유효성 검사

--- Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,15 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
     private Integer id;
+    @Size(min=2, message = "Name은 최소 2글자 이상이어야 합니다.")
     private String name;
+    @Past(message = "Birth Date는 과거의 날짜로 지정해야 합니다.")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
--- Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ Step01/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,52 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
 
 @RestController
 public class UserResource {
+    private UserDaoService service;
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+    //GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    //GET /users/{id}
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id : "+id);
+        return user;
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+        // /users/{id} <- user.getiID
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        // location 헤더. -> /users/4
+        return ResponseEntity.created(location).build();
+    }
+
+    //DELETE
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
 }
